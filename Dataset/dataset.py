fb0f43118b2503cf729d9f2ac196a00dd569c8f7
zwenwang <103382014@qq.com>
2018-11-03 19:35:28 +0800

    uncommit lmdb

--- Dataset/dataset.py
+++ Dataset/dataset.py
@@ -1,27 +1,27 @@
 from torch.utils.data import Dataset
-# import lmdb
+import lmdb
 import other
 
 
-# class LmdbDataset(Dataset):
-#     def __init__(self, root, transformer=None):
-#         self.env = lmdb.open(root, max_readers=1, readonly=True, lock=False, readahead=False, meminit=False)
-#         if not self.env:
-#             print("Cannot create lmdb from root {0}.".format(root))
-#         with self.env.begin(write=False) as e:
-#             self.data_num = int(e.get('num'))
-#         self.transformer = transformer
-#
-#     def __len__(self):
-#         return self.data_num
-#
-#     def __getitem__(self, index):
-#         assert index <= len(self), 'Index out of range.'
-#         index += 1
-#         with self.env.begin(write=False) as e:
-#             img_key = 'image-%09d' % index
-#             img_base64 = e.get(img_key)
-#             img = other.base642np_image(img_base64)
-#             gt_key = 'gt-%09d' % index
-#             gt = str(e.get(gt_key))
-#         return img, gt
+class LmdbDataset(Dataset):
+    def __init__(self, root, transformer=None):
+        self.env = lmdb.open(root, max_readers=1, readonly=True, lock=False, readahead=False, meminit=False)
+        if not self.env:
+            print("Cannot create lmdb from root {0}.".format(root))
+        with self.env.begin(write=False) as e:
+            self.data_num = int(e.get('num'))
+        self.transformer = transformer
+
+    def __len__(self):
+        return self.data_num
+
+    def __getitem__(self, index):
+        assert index <= len(self), 'Index out of range.'
+        index += 1
+        with self.env.begin(write=False) as e:
+            img_key = 'image-%09d' % index
+            img_base64 = e.get(img_key)
+            img = other.base642np_image(img_base64)
+            gt_key = 'gt-%09d' % index
+            gt = str(e.get(gt_key))
+        return img, gt
