3ca317db4ba031e554b22e7db4ce0f95d00fc213
zwenwang <103382014@qq.com>
2018-11-03 21:19:01 +0800

    fix some problem

--- generate config.py
+++ generate config.py
@@ -3,13 +3,12 @@ import ConfigParser
 
 if __name__ == '__main__':
     cp = ConfigParser.ConfigParser()
-    cp['global'] = {
-        'using_cuda': 'True',
-        'epoch': '12',
-        'gpu_id': '0',
-        'display_file_name': 'False',
-        'display_iter': '10',
-        'val_iter': '30',
-    }
+    cp.add_section('global')
+    cp.set('global', 'using_cuda', 'False')
+    cp.set('global', 'epoch', '12')
+    cp.set('global', 'gpu_id', '0')
+    cp.set('global', 'display_file_name', 'False')
+    cp.set('global', 'display_iter', '10')
+    cp.set('global', 'val_iter', '30')
     with open('./config', 'w') as fp:
         cp.write(fp)
