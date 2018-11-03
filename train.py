a69c7fc5c65c08436174595ec810edc012216e3d
zwenwang <103382014@qq.com>
2018-11-03 15:52:42 +0800

    debug

--- train.py
+++ train.py
@@ -8,7 +8,7 @@ import os
 import other
 
 
-os.environ['CUDA_VISIBLE_DEVICES'] = '6'
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 if __name__ == '__main__':
     no_grad = [
         'cnn.VGG_16.convolution1_1.weight',
@@ -17,6 +17,7 @@ if __name__ == '__main__':
         'cnn.VGG_16.convolution1_2.bias'
     ]
     epoch = 12
+    lr = 0.001
     net = Net.CTPN()
     for name, value in net.named_parameters():
         if name in no_grad:
@@ -40,10 +41,15 @@ if __name__ == '__main__':
     gt_root1 = './train_data/gt'
 
     im_list = []
-    im_list.append(os.listdir(img_root))
     im_list.append(os.listdir(img_root1))
+    im_list.append(os.listdir(img_root))
     total_iter = len(im_list[0]) + len(im_list[1])
+    optimizer = optim.SGD(net.parameters(), lr=lr, momentum=0.9, weight_decay=0.0005)
     for i in range(epoch):
+        if i >= 9:
+            lr = 0.0001
+        else:
+            lr = 0.001
         iteration = 1
         total_loss = 0
         total_cls_loss = 0
@@ -53,14 +59,15 @@ if __name__ == '__main__':
             for im in im_list[j]:
                 name, _ = os.path.splitext(im)
                 gt_name = 'gt_' + name + '.txt'
-                if j == 0:
+                if j == 1:
                     gt_path = os.path.join(gt_root, gt_name)
                 else:
                     gt_path = os.path.join(gt_root1, gt_name)
                 if not os.path.exists(gt_path):
                     print('Ground truth file of image {0} not exists.'.format(im))
 
-                if j == 0:
+                print(os.path.join(img_root, im))
+                if j == 1:
                     gt_txt = Dataset.port.read_gt_file(gt_path, have_BOM=True)
                     img = cv2.imread(os.path.join(img_root, im))
                 else:
@@ -83,11 +90,6 @@ if __name__ == '__main__':
                     negative += negative1
                     vertical_reg += vertical_reg1
                     side_refinement_reg += side_refinement_reg1
-
-                if i >= 9:
-                    optimizer = optim.SGD(net.parameters(), lr=0.0001, momentum=0.9, weight_decay=0.0005)
-                else:
-                    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9, weight_decay=0.0005)
 
                 optimizer.zero_grad()
                 loss, cls_loss, v_reg_loss, o_reg_loss = criterion(score, vertical_pred, side_refinement, positive,
